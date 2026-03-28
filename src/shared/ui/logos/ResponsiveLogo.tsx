9d13d29da03aa2ca2107fbccba1fd2c631da171d
cho1643 <govniem30@naver.com>
2026-03-28 19:18:56 +0900

    블로그 제목 변경

--- src/shared/ui/logos/ResponsiveLogo.tsx
+++ src/shared/ui/logos/ResponsiveLogo.tsx
@@ -1,102 +1,18 @@
 export const ResponsiveLogo = () => {
     return (
         <>
-            <svg
-                className="hidden h-[var(--res-logo-h)] w-[var(--res-logo-text-w)] md:block"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 288 102"
-                fill="none"
-            >
-                <path
-                    d="M76 22L71 17L76 12L81 17L76 22Z"
-                    stroke="var(--gray-12)"
-                    strokeWidth="2"
-                    strokeLinecap="square"
-                />
-                <path
-                    d="M150 72L145 67L150 62L155 67L150 72Z"
-                    stroke="var(--gray-12)"
-                    strokeWidth="2"
-                    strokeLinecap="square"
-                />
-                <path
-                    d="M65.5 92C15.5 92 15.5 42 65.5 42C115.5 42 115.5 92 65.5 92Z"
-                    stroke="var(--gray-12)"
-                    strokeWidth="2"
-                    strokeLinecap="square"
-                />
-                <path
-                    d="M160.5 92C110.5 92 110.5 42 160.5 42C210.5 42 210.5 92 160.5 92Z"
-                    stroke="var(--gray-12)"
-                    strokeWidth="2"
-                    strokeLinecap="square"
-                />
-                <path
-                    d="M234.5 66C184.5 66 184.5 16 234.5 16C284.5 16 284.5 66 234.5 66Z"
-                    stroke="var(--gray-12)"
-                    strokeWidth="2"
-                    strokeLinecap="square"
-                />
-                <path
-                    d="M273.5 13V63"
-                    stroke="var(--gray-12)"
-                    strokeWidth="2"
-                    strokeLinecap="square"
-                />
-                <path
-                    d="M224 39L213.5 87H275.5"
-                    stroke="var(--gray-12)"
-                    strokeWidth="2"
-                    strokeLinecap="square"
-                />
-                <path
-                    d="M11 17H45"
-                    stroke="var(--gray-12)"
-                    strokeWidth="2"
-                    strokeLinecap="square"
-                />
-                <path
-                    d="M106 17H174"
-                    stroke="var(--gray-12)"
-                    strokeWidth="2"
-                    strokeLinecap="square"
-                />
-                <path
-                    d="M11 58L26 18H30L51 74"
-                    stroke="var(--gray-12)"
-                    strokeWidth="2"
-                />
-                <path
-                    d="M106 58L121 18H125L135.5 46"
-                    stroke="var(--gray-12)"
-                    strokeWidth="2"
-                />
-            </svg>
+            <div className="hidden whitespace-nowrap md:block">
+                <span className="font-extrabold text-lg">
+                    에이치제이 이엔지
+                </span>
+                <span className="ml-1 text-sm text-[var(--gray-11)]">
+                    (HJ ENG)
+                </span>
+            </div>
 
-            <svg
-                className="block h-[var(--res-logo-h)] w-[var(--res-logo-symbol-w)] md:hidden"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 80 80"
-                fill="none"
-            >
-                <path
-                    d="M63.5 45L58.5 40L63.5 35L68.5 40L63.5 45Z"
-                    stroke="var(--gray-12)"
-                    strokeWidth="2"
-                    strokeLinecap="square"
-                />
-                <path
-                    d="M8 23H42"
-                    stroke="var(--gray-12)"
-                    strokeWidth="2"
-                    strokeLinecap="square"
-                />
-                <path
-                    d="M8 64L23 24H27L42 64"
-                    stroke="var(--gray-12)"
-                    strokeWidth="2"
-                />
-            </svg>
+            <div className="block md:hidden">
+                <span className="font-extrabold text-base">HJ ENG</span>
+            </div>
         </>
     );
 };
